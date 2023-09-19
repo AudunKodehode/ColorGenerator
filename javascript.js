4ba10dff7bb64c72fcfdf4c71340229bc1336424
AudunKodehode <audunkodehode@gmail.com>
2023-09-19 13:53:03 +0200

    Updated to refresh colors without refreshing site

--- javascript.js
+++ javascript.js
@@ -19,45 +19,55 @@ page.appendChild(colorContainer);
 let colorHeight = "90vh";
 let colorWidth = "20vw";
 
-for (let i = 5; i > 0; i--) {
-  let colorSegment = `element${i}`;
-  colorSegment = document.createElement("div");
-  colorSegment.style.height = `${colorHeight}`;
-  colorSegment.style.width = `${colorWidth}`;
-  let randomR = Math.floor(Math.random() * 255);
-  let randomG = Math.floor(Math.random() * 255);
-  let randomB = Math.floor(Math.random() * 255);
-  let randomRGBColor = `rgb(${randomR}, ${randomG}, ${randomB})`;
 
-  colorSegment.style.backgroundColor = randomRGBColor;  
-  colorContainer.appendChild(colorSegment);
-  let colorParagraph = document.createElement("p");
-  colorParagraph.textContent = randomRGBColor;
-  colorSegment.appendChild(colorParagraph);
-  colorSegment.style.display = "flex";
-  colorSegment.style.justifyContent = "center";
-  colorSegment.style.alignItems = "center";
-  colorSegment.style.flexDirection = "column";
-  let randomHEXColor = rgbToHex(randomR, randomG, randomB);
-  randomHEXColor = randomHEXColor.toUpperCase();
-  let colorHexParagraph = document.createElement("p");
-  colorHexParagraph.style.fontSize = "60px";
-  colorParagraph.style.fontSize = "40px";
-  colorHexParagraph.textContent = randomHEXColor;
-  colorSegment.appendChild(colorHexParagraph);
-  let span = document.createElement("span");
-  colorSegment.appendChild(span);
-  colorSegment.addEventListener("click", function () {
-    console.log(colorHexParagraph.textContent);
-    navigator.clipboard.writeText(colorHexParagraph.textContent);
-    span.style.backgroundColor = "white";
-    span.style.fontSize = "30px";
-    span.textContent = "Copied!";
-    setTimeout(() => {
-      span.textContent = "";
-    }, 1500);
-  });
+
+
+function generateColors(){
+  while (colorContainer.firstChild) {
+    colorContainer.removeChild(colorContainer.firstChild);
 }
+  for (let i = 5; i > 0; i--) {
+
+    let colorSegment = `element${i}`;
+    colorSegment = document.createElement("div");
+    colorSegment.style.height = `${colorHeight}`;
+    colorSegment.style.width = `${colorWidth}`;
+    let randomR = Math.floor(Math.random() * 255);
+    let randomG = Math.floor(Math.random() * 255);
+    let randomB = Math.floor(Math.random() * 255);
+    let randomRGBColor = `rgb(${randomR}, ${randomG}, ${randomB})`;
+    colorSegment.style.backgroundColor = randomRGBColor;  
+    colorContainer.appendChild(colorSegment);
+    let colorParagraph = document.createElement("p");
+    colorParagraph.textContent = randomRGBColor;
+    colorSegment.appendChild(colorParagraph);
+    colorSegment.style.display = "flex";
+    colorSegment.style.justifyContent = "center";
+    colorSegment.style.alignItems = "center";
+    colorSegment.style.flexDirection = "column";
+    let randomHEXColor = rgbToHex(randomR, randomG, randomB);
+    randomHEXColor = randomHEXColor.toUpperCase();
+    let colorHexParagraph = document.createElement("p");
+    colorHexParagraph.style.fontSize = "60px";
+    colorParagraph.style.fontSize = "40px";
+    colorHexParagraph.textContent = randomHEXColor;
+    colorSegment.appendChild(colorHexParagraph);
+    let span = document.createElement("span");
+    colorSegment.appendChild(span);
+    colorSegment.addEventListener("click", function () {
+      console.log(colorHexParagraph.textContent);
+      navigator.clipboard.writeText(colorHexParagraph.textContent);
+      span.style.backgroundColor = "white";
+      span.style.fontSize = "30px";
+      span.textContent = "Copied!";
+      setTimeout(() => {
+        span.textContent = "";
+      }, 1500);
+    });
+  }
+}
+generateColors()
+
 body.style.height = "100vh";
 
 page.style.display = "flex";
@@ -69,7 +79,7 @@ paragraph.style.color = "white";
 
 document.addEventListener("keydown", function (event) {     // Lukter etter tastepress og oppdaterer siden når space trykkes
   if (event.key === " ") {
-    location.reload();
+    generateColors(); 
   }
 });
 
