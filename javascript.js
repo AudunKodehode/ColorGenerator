60c08d4d9bb43da5cb007f0a9bbedd66740019f6
AudunKodehode <audunkodehode@gmail.com>
2023-09-19 14:21:48 +0200

    Added "mediaqueries" to javascript

--- javascript.js
+++ javascript.js
@@ -19,15 +19,11 @@ page.appendChild(colorContainer);
 let colorHeight = "90vh";
 let colorWidth = "20vw";
 
-
-
-
-function generateColors(){
+function generateColors() {
   while (colorContainer.firstChild) {
     colorContainer.removeChild(colorContainer.firstChild);
-}
+  }
   for (let i = 5; i > 0; i--) {
-
     let colorSegment = `element${i}`;
     colorSegment = document.createElement("div");
     colorSegment.style.height = `${colorHeight}`;
@@ -36,7 +32,7 @@ function generateColors(){
     let randomG = Math.floor(Math.random() * 255);
     let randomB = Math.floor(Math.random() * 255);
     let randomRGBColor = `rgb(${randomR}, ${randomG}, ${randomB})`;
-    colorSegment.style.backgroundColor = randomRGBColor;  
+    colorSegment.style.backgroundColor = randomRGBColor;
     colorContainer.appendChild(colorSegment);
     let colorParagraph = document.createElement("p");
     colorParagraph.textContent = randomRGBColor;
@@ -48,8 +44,11 @@ function generateColors(){
     let randomHEXColor = rgbToHex(randomR, randomG, randomB);
     randomHEXColor = randomHEXColor.toUpperCase();
     let colorHexParagraph = document.createElement("p");
-    colorHexParagraph.style.fontSize = "60px";
-    colorParagraph.style.fontSize = "40px";
+
+    colorHexParagraph.style.fontSize = "50px";
+    colorParagraph.style.fontSize = "30px";
+    colorHexParagraph.id = "hex"
+    colorParagraph.id = "rgb"
     colorHexParagraph.textContent = randomHEXColor;
     colorSegment.appendChild(colorHexParagraph);
     let span = document.createElement("span");
@@ -66,10 +65,10 @@ function generateColors(){
     });
   }
 }
-generateColors()
+generateColors();
+
 
 body.style.height = "100vh";
-
 page.style.display = "flex";
 page.style.alignItems = "center";
 page.style.flexDirection = "column";
@@ -77,16 +76,57 @@ page.style.flexDirection = "column";
 header.style.color = "white";
 paragraph.style.color = "white";
 
-document.addEventListener("keydown", function (event) {     // Lukter etter tastepress og oppdaterer siden når space trykkes
+document.addEventListener("keydown", function (event) {
+  // Lukter etter tastepress og oppdaterer siden når space trykkes
   if (event.key === " ") {
-    generateColors(); 
+    generateColors();
   }
 });
 
 function componentToHex(c) {
-    var hex = c.toString(16);
-    return hex.length == 1 ? "0" + hex : hex;
+  var hex = c.toString(16);
+  return hex.length == 1 ? "0" + hex : hex;
+}
+function rgbToHex(r, g, b) {
+  return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+}
+
+
+function reportWindowSize() {
+  let rgbElements = document.querySelectorAll("#rgb");
+  let hexElements = document.querySelectorAll("#hex");
+  console.log( window.innerWidth);
+  if (window.innerWidth > 1499) {
+    rgbElements.forEach(element => {
+      element.style.fontSize = "30px";
+    });
+    hexElements.forEach(element => {
+      element.style.fontSize = "50px";
+    })
   }
-  function rgbToHex(r, g, b) {
-    return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+  if (window.innerWidth < 1200) {
+    rgbElements.forEach(element => {
+      element.style.fontSize = "20px";
+    });
+    hexElements.forEach(element => {
+      element.style.fontSize = "40px";
+    })
   }
+  if (window.innerWidth < 900) {
+    rgbElements.forEach(element => {
+      element.style.fontSize = "20px";
+    });
+    hexElements.forEach(element => {
+      element.style.fontSize = "30px";
+    })
+  }
+  if (window.innerWidth < 750) {
+    rgbElements.forEach(element => {
+      element.style.fontSize = "10px";
+    });
+    hexElements.forEach(element => {
+      element.style.fontSize = "20px";
+    })
+  }
+}
+window.addEventListener("resize", reportWindowSize);
